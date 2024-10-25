15843d267fda2890e63129e55794473201aeb628
Shafiul islam mashruf <bemashrufislam@gmail.com>
2024-10-25 20:33:06 +0600

    longestCommonSubsequence

---
+++ Longest _Common_Subsequence.cpp
@@ -0,0 +1,50 @@
+// Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+// For example, "ace" is a subsequence of "abcde".
+// A common subsequence of two strings is a subsequence that is common to both strings.
+
+// MASHRUF's work
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define ff first
+#define mp make_pair
+#define no cout<<"NO"<<'\n'
+#define yes cout<<"YES"<<'\n'
+#define endl '\n'
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+
+int ans(string &a, int n, string &b, int m, vector<vector<int>> &dp) {
+        if (n == 0 || m == 0) return 0;
+        if (dp[n][m] != -1) return dp[n][m];
+        
+        if (a[n-1] == b[m-1]) {
+            dp[n][m] = 1 + ans(a, n-1, b, m-1,dp);
+        } else {
+            int ans1 = ans(a, n-1, b, m, dp);
+            int ans2 = ans(a, n, b, m-1, dp);
+            dp[n][m] = max(ans1, ans2);
+        }
+        
+        return dp[n][m];
+    }
+
+    int longestCommonSubsequence(string a, string b) {
+        int n = a.size();
+        int m = b.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return ans(a, n, b, m, dp);
+    }
+
+int main()
+{
+    fast();
+    string a,b;
+    cin>>a>>b;
+    cout<< longestCommonSubsequence(a, b);
+
+    return 0;
+}
--- coin_change_leetcode.cpp
+++ coin_change_leetcode.cpp
@@ -10,45 +10,91 @@ using namespace std;
 #define pb push_back
 #define ff first
 #define mp make_pair
-#define no cout<<"NO"<<'\n'
-#define yes cout<<"YES"<<'\n'
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
 #define endl '\n'
 #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
+int coinChange(vector<int> &coins, int amount)
+{
+    vector<int> dp(amount + 1, INT_MAX - 1);
+    dp[0] = 0;
 
-int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX - 1);
-        dp[0] = 0;
-
-        for (auto coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] = min(dp[j], 1 + dp[j - coin]);
-            }
+    for (auto coin : coins)
+    {
+        for (int j = coin; j <= amount; j++)
+        {
+            dp[j] = min(dp[j], 1 + dp[j - coin]);
         }
-        return dp[amount] == INT_MAX - 1 ? -1 : dp[amount];
     }
+    return dp[amount] == INT_MAX - 1 ? -1 : dp[amount];
+}
 
 int main()
 {
     fast();
     int count;
-    cin>>count;
-    vector<int>coins(count);
+    cin >> count;
+    vector<int> coins(count);
     for (int i = 0; i < count; i++)
     {
-        cin>>coins[i];
+        cin >> coins[i];
     }
     int amount;
-    cin>>amount;
+    cin >> amount;
 
-    int ans = coinChange(coins,amount);
-    cout<<ans<<endl;
+    int ans = coinChange(coins, amount);
+    cout << ans << endl;
 
     return 0;
 }
 
+// another approach
 
+int coinChange(vector<int> &w, int s)
+{
+    int n = w.size();
+    int dp[n + 1][s + 1];
 
-//another approach
+    dp[0][0] = 0;
+    for (int i = 1; i <= s; i++)
+        dp[0][i] = INT_MAX - 1;
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            if (w[i - 1] <= j)
+            {
+                dp[i][j] = min(1 + dp[i][j - w[i - 1]], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    if (dp[n][s] == INT_MAX - 1)
+        return -1;
+    else
+        return dp[n][s];
+}
 
+int main()
+{
+    fast();
+    int count;
+    cin >> count;
+    vector<int> coins(count);
+    for (int i = 0; i < count; i++)
+    {
+        cin >> coins[i];
+    }
+    int amount;
+    cin >> amount;
+
+    int ans = coinChange(coins, amount);
+    cout << ans << endl;
+
+    return 0;
+}
